a4a09b82de9f308e5dd0ae24f9b0c0c664be0f1a
salsarizki <salsarizkisaputrii@gmail.com>
2024-05-05 23:43:44 +0700

    new update

---
+++ Week 4/PPW24-04_L0122147_SalsaRizkiSaputri/SourceCode/index.php
@@ -0,0 +1,66 @@
+<!-- Nama = Salsa Rizki Saputri
+NIM  = L0122147 -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pola Pattern</title>
+    <style>
+        .lightGreen-box {
+            width: 175px;
+            height: 175px;
+            background-color: #D9EDBF;
+            display: inline-block;
+        }
+
+        .darkGreen {
+            width: 175px;
+            height: 175px;
+            background-color: #2C7865;
+            display: inline-block;
+        }
+
+        .white-box {
+            width: 175px;
+            height: 175px;
+            background-color: white;
+            display: inline-block;
+        }
+
+        div {
+            margin: 0;
+            padding: 0;
+        }
+
+    </style>
+</head>
+<body>
+    <?php
+    $rows = 5;
+
+    for ($i = $rows; $i > 0; $i--) {
+        echo '<div>';
+        for ($j = $rows; $j > 0; $j--) {
+            if ($j > $rows - $i + 1){
+                echo '<div class="white-box"></div>';
+            } else {
+                if ($i % 2 == 0) {
+                    if ($j % 2 == 0) {
+                        echo '<div class="lightGreen-box"></div>';
+                    } else {
+                        echo '<div class="darkGreen"></div>';
+                    }
+                } else {
+                    if ($j % 2 == 0) {
+                        echo '<div class="darkGreen"></div>';
+                    } else {
+                        echo '<div class="lightGreen-box"></div>';
+                    }
+                }
+            }
+        }
+        echo '</div>';
+    }
+    ?>
+</body>
+</html>
